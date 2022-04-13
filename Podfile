7c7c8c4fa46a4ec9888b43be3dce9d8d4804da4e
Andrey Dorofeev <adorofeev@gmail.com>
2022-04-13 18:27:11 +0700

    added packageKey model

--- Podfile
+++ Podfile
@@ -5,5 +5,6 @@ target 'SIOnline' do
 
 pod 'R.swift', '6.1.0'
 pod 'Alamofire', '5.5'
+pod 'PromiseKit', '6.8'
 
 end
---
+++ SIOnline/Models/Server/PackageKey.swift
@@ -0,0 +1,12 @@
+//
+//  PackageKey.swift
+//  SIOnline
+//
+//  Created by Andrey Dorofeev on 13.04.2022.
+//
+
+struct PackageKey {
+    let name: String?
+    let hash: String?
+    let id: String?
+}
