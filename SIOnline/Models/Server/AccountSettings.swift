c8d2b98bf5626a4ee0e9340ded4473b2dedf6e57
Andrey Dorofeev <adorofeev@gmail.com>
2022-04-14 10:23:43 +0700

    added Account Settings model

---
+++ SIOnline/Models/Server/AccountSettings.swift
@@ -0,0 +1,12 @@
+//
+//  AccountSettings.swift
+//  SIOnline
+//
+//  Created by Andrey Dorofeev on 14.04.2022.
+//
+
+struct AccountSettings {
+    let name: String
+    let isHuman: Bool
+    let isMale: Bool
+}
