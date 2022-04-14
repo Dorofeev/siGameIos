f01f2c5a7e5a07bc00cf9cc13084340fa8395364
Andrey Dorofeev <adorofeev@gmail.com>
2022-04-14 10:30:19 +0700

    added Server app settings model

---
+++ SIOnline/Models/Server/ServerAppSettings.swift
@@ -0,0 +1,20 @@
+//
+//  ServerAppSettings.swift
+//  SIOnline
+//
+//  Created by Andrey Dorofeev on 14.04.2022.
+//
+
+struct ServerAppSettings {
+    let timeSettings: TimeSettings
+    let readingSpeed: Int
+    let falseStart: Bool
+    let hintShowman: Bool
+    let oral: Bool
+    let ignoreWrong: Bool
+    let gameMode: String
+    let randomQuestionsBasePrice: Int
+    let randomRoundsCount: Int
+    let randomThemesCount: Int
+    let culture: String
+}
