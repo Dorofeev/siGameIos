ec8d53168948fd23a717b39ee67d277938d76457
Andrey Dorofeev <adorofeev@gmail.com>
2022-04-15 14:16:32 +0700

    added Data context

--- Podfile
+++ Podfile
@@ -6,5 +6,6 @@ target 'SIOnline' do
 pod 'R.swift', '6.1.0'
 pod 'Alamofire', '5.5'
 pod 'PromiseKit', '6.8'
+pod 'SwiftSignalRClient', '0.9.0'
 
 end
--- SIOnline/Models/DataContext.swift
+++ SIOnline/Models/DataContext.swift
@@ -4,8 +4,12 @@
 //
 //  Created by Andrey Dorofeev on 13.04.2022.
 //
+import SwiftSignalRClient
 
 struct DataContext {
     let config: Config
     let serverUri: String
+    let connection: HubConnection?
+    let gameClient: IGameServerClient
+    let contentUris: [String]
 }
