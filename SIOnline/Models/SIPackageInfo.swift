aa7a28e0ad05a284e360a5b8ecb18ae59014c126
Andrey Dorofeev <adorofeev@gmail.com>
2022-04-15 16:18:27 +0700

    added si package info

---
+++ SIOnline/Models/SIPackageInfo.swift
@@ -0,0 +1,19 @@
+//
+//  SIPackageInfo.swift
+//  SIOnline
+//
+//  Created by Andrey Dorofeev on 15.04.2022.
+//
+
+struct SIPackageInfo {
+    let authors: String
+    let description: String
+    let difficulty: Int
+    let guid: String
+    let restriction: RestrictionType
+    let id: Int
+    let logo: String
+    let publishedDate: String
+    let publisher: String
+    let tags: String
+}
