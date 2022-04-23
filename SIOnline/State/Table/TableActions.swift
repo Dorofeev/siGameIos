d60c968478286647facdafa3ce2a95822886fcb2
Genichiro1 <koiwright666@gmail.com>
2022-04-23 14:36:00 +0700

    added TableActions

---
+++ SIOnline/State/Table/TableActions.swift
@@ -0,0 +1,31 @@
+//
+//  TableActions.swift
+//  SIOnline
+//
+//  Created by Anna Kuptsova on 23.04.2022.
+//
+
+typealias KnownTableAction = TableActionTypes
+
+enum TableActionTypes {
+    case showLogo
+    case showGameThemes(gameThemes: [String])
+    case showRoundThemes(roundThemes: [ThemeInfo], isFinal: Bool, display: Bool)
+    case showText(text: String, animateReading: Bool)
+    case showAnswer(text: String)
+    case showRoundTable
+    case blinkQuestion(themeIndex: Int, questionIndex: Int)
+    case blinkTheme(themeIndex: Int)
+    case removeQuestion(themeIndex: Int, questionIndex: Int)
+    case removeTheme(themeIndex: Int)
+    case showPartialText(textShape: String)
+    case appendPatrialText(text: String)
+    case showImage(uri: String)
+    case showAudio(uri: String)
+    case showVideo(uri: String)
+    case showSpecial(text: String, activeThemeIndex: Int)
+    case canPressChanged(canPress: Bool)
+    case isSelectableChanged(isSelectable: Bool)
+    case resumeMedia
+    case captionChanged(caption: String)
+}
