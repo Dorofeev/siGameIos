d6148028240ca499b5f848e1dd7b752e6929d9ee
Andrey Dorofeev <adorofeev@gmail.com>
2022-04-24 11:52:21 +0700

    added constants

---
+++ SIOnline/Models/Enums /Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  SIOnline
+//
+//  Created by Andrey Dorofeev on 24.04.2022.
+//
+
+enum Constants {
+    static let keyEnter = 13
+    static let keyCtrl = "Control"
+    static let keyEscape = 27
+    static let anyName = " "
+    static let maxPlayerCount = 12
+}
+
