40d06bf748c922fb71ed93866d857d5ea2bf7d19
Andrey Dorofeev <adorofeev@icerockdev.com>
2022-06-18 12:06:47 +0700

    SIO-11 added player handler

--- SIOnline/State/Game/MessageProcessor.swift
+++ SIOnline/State/Game/MessageProcessor.swift
@@ -11,29 +11,26 @@ import ReSwiftThunk
 class MessageProcessor {
     static var lastReplicLock: Int = 0
     
-    static func messageProcessor(dispatch: DispatchFunction, message: ChatMessage) {
+    static func messageProcessor(dispatch: DispatchFunction, message: ChatMessage, dataContext: DataContext) {
         if message.isSystem {
-            dispatch(processSystemMessage(message))
+            dispatch(processSystemMessage(message, dataContext))
             return
         }
         
         dispatch(userMessageReceived(message))
     }
     
-    static let processSystemMessage: (ChatMessage) -> Thunk<State> = { message in
+    static let processSystemMessage: (ChatMessage, DataContext) -> Thunk<State> = { message, dataContext in
         Thunk { dispatch, state in
             guard let state = state() else { return }
             let role = state.run.role
             let args = message.text.components(separatedBy: "\n")
             
-            // TODO: - not yet implemented
-            // viewerHandler(dispatch, state, dataContext, args);
+            viewerHandler(dispatch, state, dataContext, args)
             
             switch role {
             case .Player:
-                // TODO: - not yet implemented
-                //playerHandler(dispatch, state, dataContext, args);
-                break
+                playerHandler(dispatch, state, dataContext, args)
             case .Showman:
                 // TODO: - not yet implemented
                 // showmanHandler(dispatch, state, dataContext, args);
@@ -494,10 +491,126 @@ class MessageProcessor {
                         // TODO: - not yet implemented
                         // dispatch(runActionCreators.clearDecisionsAndMainTimer());
                     }
+                case "PAUSE":
+                    // TODO: - not yet implemented
+                    // dispatch(runActionCreators.pauseTimer(timerIndex, timerArgument, false));
+                    break
+                case "USER_PAUSE":
+                    // TODO: - not yet implemented
+                    // dispatch(runActionCreators.pauseTimer(timerIndex, timerArgument, true));
+                    break
+                case "RESUME":
+                    // TODO: - not yet implemented
+                    // dispatch(runActionCreators.resumeTimer(timerIndex, false));
+                    break
+                case "USER_RESUME":
+                    // TODO: - not yet implemented
+                    // dispatch(runActionCreators.resumeTimer(timerIndex, true));
+                    break
+                case "MAXTIME":
+                    // TODO: - not yet implemented
+                    // dispatch(runActionCreators.timerMaximumChanged(timerIndex, timerArgument));
+                    break
                 default:
                     break
                 }
             }
+        case "THEME" where args.count > 1:
+            // TODO: - not yet implemented
+            // dispatch(runActionCreators.playersStateCleared());
+            // dispatch(runActionCreators.showmanReplicChanged(''));
+            
+            dispatch(TableActionCreators.showText("\(R.string.localizable.theme): \(secondArg)", false))
+            
+            // TODO: - not yet implemented
+            // dispatch(runActionCreators.afterQuestionStateChanged(false));
+            // dispatch(runActionCreators.themeNameChanged(args[1]));
+        case "TRY":
+            dispatch(TableActionCreators.canPressChanged(true))
+        case "WINNER":
+            // TODO: process
+            break
+        case "WRONGTRY":
+            guard let index = secondArg.toInt() else { return }
+            let players = state.run.persons.players
+            if index > -1 && index < players.count {
+                let player = players[index]
+                if player.state == .none {
+                    // TODO: - not yet implemented
+                    // dispatch(runActionCreators.playerStateChanged(index, PlayerStates.Lost));
+                    // setTimeout( () => { dispatch(runActionCreators.playerLostStateDropped(index)); }, 200 )
+                }
+            }
+        default:
+            break
+        }
+    }
+    
+    static let playerHandler: (DispatchFunction, State, DataContext, [String]) -> Void = { dispatch, state, dataContext, args in
+        guard !args.isEmpty else { return }
+        switch args[0] {
+        case "ANSWER":
+            // TODO: - not yet implemented
+            // dispatch(runActionCreators.isAnswering());
+            break
+        case "CANCEL":
+            // TODO: - not yet implemented
+            // dispatch(runActionCreators.clearDecisions());
+            break
+        case "CAT":
+            // TODO: - not yet implemented
+            // const indices = getIndices(args);
+            // dispatch(runActionCreators.selectionEnabled(indices, 'CAT'));
+            break
+        case "CATCOST":
+            let allowedStakeTypes = [StakeTypes.nominal: false, .sum: true, .pass: false, .allIn: false]
+            
+            guard let minimum = args.getSafe(1)?.toInt(),
+                  let maximum = args.getSafe(2)?.toInt(),
+                  let step = args.getSafe(3)?.toInt() else { return }
+            
+            // TODO: - not yet implemented
+            // dispatch(runActionCreators.setStakes(allowedStakeTypes, minimum, maximum, minimum, step, 'CATCOST', true));
+            // dispatch(runActionCreators.decisionNeededChanged(true));
+        case "CHOOSE":
+            // TODO: - not yet implemented
+            // dispatch(runActionCreators.decisionNeededChanged(true));
+            
+            dispatch(TableActionCreators.isSelectableChanged(true))
+        case "FINALSTAKE":
+            // TODO: - not yet implemented
+            // const me = getMe(state);
+            // if (!me) { break; }
+            
+            let allowedStakeTypes = [StakeTypes.nominal: false, .sum: true, .pass: false, .allIn: false]
+            
+            // TODO: - not yet implemented
+            // dispatch(runActionCreators.setStakes(allowedStakeTypes, 1, me.sum, 1, 1, 'FINALSTAKE', true));
+            // dispatch(runActionCreators.decisionNeededChanged(true));
+        case "REPORT":
+            // TODO: process
+            break
+        case "STAKE":
+            let allowedStakeTypes = [
+                StakeTypes.nominal: args.getSafe(1) == "+",
+                .sum: args.getSafe(2) == "+",
+                .pass: args.getSafe(3) == "+",
+                .allIn: args.getSafe(4) == "+"
+            ]
+            
+            guard let minimum = args.getSafe(5) else { return }
+            
+            // TODO: - not yet implemented
+            // const me = getMe(state);
+            // if (!me) { return; }
+            
+            // TODO: - not yet implemented
+            // dispatch(runActionCreators.setStakes(allowedStakeTypes, minimum, me.sum, minimum, 100, 'STAKE', false));
+            // dispatch(runActionCreators.decisionNeededChanged(true));
+        case "VALIDATION":
+            // TODO: - not yet implemented
+            // startValidation(dispatch, localization.apellation, args);
+            break
         default:
             break
         }
