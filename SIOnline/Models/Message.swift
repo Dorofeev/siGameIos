fc1ba077c972c362bf87659812916a303f34ef83
Genichiro1 <koiwright666@gmail.com>
2022-07-16 15:55:44 +0700

    SIO-10 added Message

---
+++ SIOnline/Models/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  SIOnline
+//
+//  Created by Anna Kuptsova on 16.07.2022.
+//
+struct Message {
+    var isSystem: Bool
+    var sender: String
+    var text: String
+}
+
