4ffc9e61e1bc8ea54c32bd8196c906e52427eda6
Andrey Dorofeev <adorofeev@icerockdev.com>
2022-11-04 17:37:40 +0700

    SIO-27 added game info view controller

---
+++ SIOnline/Components/GameInfoView/GameInfoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GameInfoViewController.swift
+//  SIOnline
+//
+//  Created by Andrey Dorofeev on 04.11.2022.
+//
+
+import UIKit
+
+class GameInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
