0504920a1262f5d0c87bebebd49e47866d1c7173
Andrey Dorofeev <adorofeev@icerockdev.com>
2022-11-06 22:06:07 +0700

    SIO-27 added inner info view

--- SIOnline/Components/GameInfoView/GameInfoViewController.swift
+++ SIOnline/Components/GameInfoView/GameInfoViewController.swift
@@ -8,8 +8,31 @@
 import UIKit
 
 class GameInfoViewController: UIViewController {
+    
+    // color: white;
+    
+    // MARK: - Views
+    
+    private lazy var innerInfoView: UIView = {
+        let view = UIView()
+        
+        return view
+    }()
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+    
+    // MARK: - Setup
+    
+    private func setup() {
+        view.backgroundColor = R.color.gameinfoBackground()
+    }
+    
+    private func setupLayout() {
+        view.addEnclosedSubview(innerInfoView, insets: NSDirectionalEdgeInsets(top: 30, leading: 0, bottom: 0, trailing: 0))
     }
 }
